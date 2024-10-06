2e9753dab6c1d19fe59e3bdc5456b96781761e9a
Faru Code <161667014+faru-cody@users.noreply.github.com>
2024-10-06 09:24:56 +0530

    Update slider.js

--- own/slider.js
+++ own/slider.js
@@ -1,4 +1,4 @@
- <script>
+
         const BLOG_URL = 'https://newonetry-demo.blogspot.com'; // Replace with your blog URL
         const NUM_POSTS = 5; // Number of random posts to display
         const AUTO_SLIDE_INTERVAL = 3000; // 3 seconds for auto slide
@@ -87,4 +87,4 @@
 
         // Initialize the carousel on page load
         loadPostsIntoCarousel();
-    </script>
+
